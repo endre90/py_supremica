162a75018a7d2df3637db0e1b5ab210fc3762752
endre90 <endre90@gmail.com>
2019-06-10 21:45:24 +0200

    cleaner example

--- src/example1.py
+++ src/example1.py
@@ -1,11 +1,13 @@
 from automata_generator import Generator
 from automata_synchronizer import Synchronizer
 
+# Define automata
 states1 = ['s1', 's2', 's3']
 states2 = ['s1', 's2', 's3']
 alphabet1 = ['e1', 'e2']
 alphabet2 = ['e1', 'e3']
-u_alphabet = []
+u_alphabet1 = []
+u_alphabet2 = []
 arcs1 = [('t1', 's1', 's2', 'e1'), ('t2', 's2', 's3', 'e2')]
 arcs2 = [('t1', 's1', 's2', 'e1'), ('t4', 's2', 's3', 'e3')]
 init1 = 's1'
@@ -15,29 +17,12 @@ marked2 = ['s3']
 forbidden1 = []
 forbidden2 = []
 
+# Generate automata
 g = Generator()
-aut1 = g.generate_automaton('aut1', states1, alphabet1, u_alphabet, arcs1, init1, marked1, forbidden1)
-#print(aut1.getStates())
-aut2 = g.generate_automaton('aut2', states2, alphabet2, u_alphabet, arcs2, init2, marked2, forbidden2)
+aut1 = g.generate_automaton('aut1', states1, alphabet1, u_alphabet1, arcs1, init1, marked1, forbidden1)
+aut2 = g.generate_automaton('aut2', states2, alphabet2, u_alphabet2, arcs2, init2, marked2, forbidden2)
 
-# Some tryouts
-# print(aut1.getStates())
-# print(aut1.getAlphabet())
-# print(aut1.getTransitions())
-# print(aut1.getAlphabet().getUncontrollableAlphabet())
-# print(aut1.getAlphabet().getControllableAlphabet())
-
-# for s in aut1.getStates():
-#     if s.isAccepting():
-#         print("marked")
-#         print(s)
-#     if s.isInitial():
-#         print("initial")
-#         print(s)
-#     if s.isForbidden():
-#         print("forbidden")
-#         print(s)
-
+# Synchronize automata
 s = Synchronizer()
 auts = s.synchronize_automata([aut1, aut2], "FULL")
 print(auts.getStates())
